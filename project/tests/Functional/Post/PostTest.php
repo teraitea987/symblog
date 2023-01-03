1d076945f5cb2a6f9ff7810e450c0408d5318712
Teraitea Tarihaa <teraitea.tarihaa@protonmail.com>
2023-01-03 10:05:41 +0100

    pagination test OK

--- project/tests/Functional/Post/PostTest.php
+++ project/tests/Functional/Post/PostTest.php
@@ -19,4 +19,25 @@ class PostTest extends WebTestCase
 		$this->assertSelectorExists('h1');
 		$this->assertSelectorTextContains('h1', 'Symblog : Le blog créé de A à Z avec Symfony');
 	}
+
+	public function testPaginationWorks(): void 
+	{
+		$client = static::createClient();
+		$crawler = $client->request(Request::METHOD_GET, '/');
+
+		$this->assertResponseIsSuccessful();
+		$this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+		$posts = $crawler->filter('div.card');
+		$this->assertEquals(9, count($posts));
+
+		$link = $crawler->selectLink('2')->extract(['href'])[0];
+		$crawler = $client->request(Request::METHOD_GET, $link);
+
+		$this->assertResponseIsSuccessful();
+		$this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+		$posts = $crawler->filter('div.card');
+		$this->assertGreaterThanOrEqual(1, count($posts));
+	}
 }
